da132f199bc005143e933e114f18cf8f8c667954
divya116 <56511764+divya116@users.noreply.github.com>
2020-06-06 17:34:07 +0530

    Create Day6-majority-element.cpp

---
+++ Day6-majority-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums)
+    {
+        int candidate;//=nums[0];
+        int count=0;
+        for (auto i:nums)
+        {
+            if (count==0)
+                candidate=i;
+            if (i==candidate)
+                count++;
+            else
+                count--;  
+        }
+        return candidate;
+    }
+};
